c6457acff0829dd5de5d84c703e49aedfca64726
cameronkloot <cameron@0ptimus.com>
2016-10-06 15:40:38 -0400

    Prefix common functions

--- lib/common.php
+++ lib/common.php
@@ -5,10 +5,11 @@ function ck_encode_twitter( $text ) {
   return htmlspecialchars(urlencode(html_entity_decode($text, ENT_COMPAT, 'UTF-8')), ENT_COMPAT, 'UTF-8');
 }
 
-function startsWith($haystack, $needle) {
+function ck_starts_with( $haystack, $needle ) {
   return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
 }
-function endsWith($haystack, $needle) {
+
+function ck_end_with( $haystack, $needle ) {
   return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
 }
 
