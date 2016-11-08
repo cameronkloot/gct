56278b643ae7d390e143a0b9eee2f4c75db38da5
cameronkloot <cameron@0ptimus.com>
2016-11-08 10:44:46 -0500

    Fix quick press dashboard widget slug

--- lib/admin.php
+++ lib/admin.php
@@ -89,7 +89,7 @@ function pd_disable_dashboard_widgets() {
   remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
   remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
   remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-  remove_meta_box( 'dashboard_quigct_press', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
   remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
   remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
   remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
