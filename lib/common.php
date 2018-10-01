387d7eb6afef59629707286a458cd5dab018b237
cameronkloot <cameron@0ptimus.com>
2018-10-01 16:52:16 -0400

    Add helper functions

--- lib/common.php
+++ lib/common.php
@@ -13,4 +13,29 @@ function gct_end_with( $haystack, $needle ) {
   return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
 }
 
+function ck_heading( $heading ) {
+  if ( $heading ):
+  ?>
+  <div class="heading-container">
+    <h2 class="heading"><?php echo wp_kses_post( $heading ); ?></h2>
+  </div>
+  <?php
+  endif;
+}
+
+function ck_button( $text, $link ) {
+  if ( $text && $link ):
+  ?>
+  <div class="button-container">
+    <a class="button" href="<?php echo esc_url( $link ); ?>"><?php echo esc_html( $text ); ?></a>
+  </div>
+  <?php
+  endif;
+}
+
+function ck_image_url( $image_id, $size = 'hero' ) {
+  $image_array = wp_get_attachment_image_src( $image_id, $size );
+  return $image_array !== false ? $image_array[0] : '';
+}
+
 //: END lib/common.php ://
